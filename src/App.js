2cac0df87b583b9e54ca3fb18c8da5a1e3133943
Your Name <you@example.com>
2021-08-17 15:52:06 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+// import React from 'react';
+// import { ChatEngine } from 'react-chat-engine';
+// import ChatFeed from './components/ChatFeed';
+
+// import './App.css';
+// import LoginForm from './components/LoginForm';
+
+// function App() {
+
+//   if (!localStorage.getItem('username: ')) return <LoginForm />;
+
+//   return (
+//     <>
+//       <ChatEngine
+//         height="100vh"
+//         projectID="4dd2a19b-2ecc-4352-8cfe-80debb0446b6"
+//         userName={localStorage.getItem('username')}
+//         userSecret={localStorage.getItem('password')}
+//         renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+//       />
+//     </>
+//   );
+// }
+
+// export default App;
+
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
-function App() {
+const projectID = '4dd2a19b-2ecc-4352-8cfe-80debb0446b6';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
   );
-}
+};
+
+// infinite scroll, logout, more customizations...
 
 export default App;
